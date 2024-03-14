4e5e636de578d8eea34b392bbee20cc40de54bb3
GTU9 <sangik0909@gmail.com>
2024-03-14 10:18:21 +0900

    itellij java 중첩 조건문 예시

---
+++ lecture-source/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application {
+    public static void main(String[] args) {
+        A_for a= new A_for();
+        /*단독 for문 흐름*/
+        a.testSimpleForStatmement();
+    }
+}
