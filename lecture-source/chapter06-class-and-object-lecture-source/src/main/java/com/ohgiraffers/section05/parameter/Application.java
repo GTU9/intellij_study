553daba455b60420af925646d4182eae93aa2991
GTU9 <sangik9999@naver.com>
2024-03-21 13:41:10 +0900

    java intellij 객체지향 프로그래밍 파라미터 활용과 종류

--- lecture-source/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/parameter/Application.java
+++ lecture-source/chapter06-class-and-object-lecture-source/src/main/java/com/ohgiraffers/section05/parameter/Application.java
@@ -20,5 +20,45 @@ public class Application {
         /*기본자료형 8가지 모두 가능하다.*/
         int num=20;
         pt.testPrimaryTypeParameter(num);
+
+        /*2. 기본자료형 배열을 매개변수로 전달 받는 메소드 호출 확인*/
+        int[] iarr =new int[]{1,2,3,4,5};
+
+        System.out.print("인자로 전달하는 값 : "+iarr);
+        pt.testPrimaryTypeParameter(iarr);
+
+        /*메소드 호출 후 값 변경 확인*/
+        System.out.println("변경 후 배열의 값 출력 : ");
+        for(int i=0;i<iarr.length;i++){
+            System.out.print(iarr[i]+" ");
+        }
+        System.out.println();
+
+        /*3. 클래스 자료형*/
+        /*사각형의 가로, 세로의 길이를 저장할 RectAngle이라는 클래스를 하나 추가한다.*/
+        RectAngle r1=new RectAngle(12.5,22.5);
+
+        System.out.print("인자로 전달하는 값 : "+r1);
+        pt.testClassTypeParameter(r1);
+
+        /*메소드 호출 후 값 변경 확인*/
+        System.out.println("변경 후 사각형의 넒이와 둘레=========================");
+        r1.calcArea();
+        r1.calcRound();
+
+        /*4. 가변인자*/
+        /*인자로 전달하는 값이 갯수가 정해지지 않은 경우 가변배열을 활용할 수 있다.*/
+//        pt.testVariableLengthArrayParameter();            //인자가 아무것도 없는 경우 에러 발생
+        pt.testVariableLengthArrayParameter("홍길동");
+        pt.testVariableLengthArrayParameter("유관순", "볼링");
+        pt.testVariableLengthArrayParameter("이순신","볼링","축구","당구");
+        pt.testVariableLengthArrayParameter("신사임당",new String[]{"테니스","서예","떡썰기"});
+
+        /*주의사항. 가변인자는 메소드 오버로딩시 유의해야한다.*/
+        /*메소드를 오버로딩하고 다시 돌아오면 위에 작성한 호출 구문에 컴파일에러가 발생한다.
+        * 둘 중 어떤 메소드를 호출하는 것인지에 대해 모호성이 발생했기 떄문에 에러가 나는 것이다.
+        * 이렇게 가변배열을 매개변수로 이용한 메소드는 모호성으로 인해 오버로딩 하지 않는 것이 좋다.
+        * */
     }
+
 }
