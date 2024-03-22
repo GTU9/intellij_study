bfe2913a3e83e644de99e082e0728a3999f4f2c9
GTU9 <sangik9999@naver.com>
2024-03-22 09:31:29 +0900

    java intellij 객체배열의 여러가지 초기화 방법

---
+++ lecture-source/chap07-object-array-lecture-source/src/main/java/com/ohgiraffers/section02/uses/Application.java
@@ -0,0 +1,10 @@
+package com.ohgiraffers.section02.uses;
+
+public class Application {
+    public static void main(String[] args) {
+        /*객체배열(클래스 자료형 배열)을 매개변수와 리턴값으로 사용할 수 있따.*/
+        /*동일한 타입의 여러 갱의 인스턴스를 인자로 전달하거나 리턴받는 경우에도
+        * 객체 배열을 이용할 수 있다.
+        * */
+    }
+}
