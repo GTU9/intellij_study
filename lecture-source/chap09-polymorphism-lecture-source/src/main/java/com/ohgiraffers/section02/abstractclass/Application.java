9b8a0b10bcc33897db6f237d89bdb25ca32dd376
GTU9 <sangik9999@naver.com>
2024-03-26 13:16:32 +0900

    추상클래스 추상메소드 abstract

--- lecture-source/chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section02/abstractclass/Application.java
+++ lecture-source/chap09-polymorphism-lecture-source/src/main/java/com/ohgiraffers/section02/abstractclass/Application.java
@@ -30,7 +30,38 @@ public class Application {
 		 *  예) public abstract void method();
 		 *  */
 
-		/*설명. */
+		/* 설명. Product 클래스로 인스턴스를 생성할 수 없다. */
+//		Product product = new Product();	//추상클래스는 인스턴스 생성 불가
+
+		/* 설명. 추상클래스를 상속받은 객체를 이용해야 한다. */
+		SmartPhone smartPhone = new SmartPhone();
+
+		/* 설명. SmartPhone은 SmartPhone 타입이기도 하지만 Product 타입이기도 하다. */
+		System.out.println(smartPhone instanceof SmartPhone);
+		System.out.println(smartPhone instanceof Product);
+
+		/* 설명. 따라서 다형성을 적용해서 추상클래스를 레퍼런스 타입으로 활용할 수 있다. */
+		Product product = new SmartPhone();
+
+		/* 설명. 동적 바인딩에 의해 SmartPhone의 메소드가 호출된다. */
+		product.abstMethod();
+
+		/* 설명. 추상클래스가 가진 메소드도 호출할 수 있다. */
+		product.nonStaticMethod();
+
+		/* 설명. static 메소드는 그냥 사용이 가능하다. (인스턴스 생성 불필요) */
+		Product.staticMethod();
+
+		/* 필기.
+		 *  추상클래스를 왜 쓰나?
+		 *  추상클래스의 추상메소드는 오버라이딩에 대한 강제성이 부여된다.
+		 *  따라서 여러 클래스들을 그룹화 하여
+		 *  필수 기능을 정의하여 강제성을 부여해 개발 시 일관된 인터페이스를 제공할 수 있다.
+		 *
+		 * 필기.
+		 *  하지만 다른 클래스를 상속 받고 있는 클래스를 작성할 시에는 추상클래스를 추가로 상속받을 수 없다.
+		 *  그래서 추상클래스보다 더 강제성이 강한 인터페이스(interface)라는 매커니즘을 제공하고 있다.
+		 * */
 
 
 	}
