78f7c816d5ed638c6cc520123e99ed0fe1744c47
GTU9 <sangik9999@naver.com>
2024-03-29 10:02:53 +0900

    employee 변수초기화 getter,setter

---
+++ lecture-source/02_jdbc/chap02-statements-lecture-source/src/main/java/com/ohgiraffers/domain/entity/Empolyee.java
@@ -0,0 +1,155 @@
+package com.ohgiraffers.domain.entity;
+
+import java.sql.Date;
+
+public class Empolyee {
+
+    private String empId;
+    private String empName;
+
+    private String empNo;
+    private String email;
+
+    private String phone;
+    private String deptCode;
+    private String jobCode;
+    private String salLevel;
+    private int salary;
+    private double bonus;
+    private String manageId;
+    private java.sql.Date hireDate;
+    private java.sql.Date entDate;
+    private String entyn;
+
+    public String getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(String empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(String empNo) {
+        this.empNo = empNo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDeptCode() {
+        return deptCode;
+    }
+
+    public void setDeptCode(String deptCode) {
+        this.deptCode = deptCode;
+    }
+
+    public String getJobCode() {
+        return jobCode;
+    }
+
+    public void setJobCode(String jobCode) {
+        this.jobCode = jobCode;
+    }
+
+    public String getSalLevel() {
+        return salLevel;
+    }
+
+    public void setSalLevel(String salLevel) {
+        this.salLevel = salLevel;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public String getManageId() {
+        return manageId;
+    }
+
+    public void setManageId(String manageId) {
+        this.manageId = manageId;
+    }
+
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(Date hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public Date getEntDate() {
+        return entDate;
+    }
+
+    public void setEntDate(Date entDate) {
+        this.entDate = entDate;
+    }
+
+    public String getEntyn() {
+        return entyn;
+    }
+
+    public void setEntyn(String entyn) {
+        this.entyn = entyn;
+    }
+
+    @Override
+    public String toString() {
+        return "Empolyee{" +
+                "empId='" + empId + '\'' +
+                ", empName='" + empName + '\'' +
+                ", empNo='" + empNo + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", deptCode='" + deptCode + '\'' +
+                ", jobCode='" + jobCode + '\'' +
+                ", salLevel='" + salLevel + '\'' +
+                ", salary=" + salary +
+                ", bonus=" + bonus +
+                ", manageId='" + manageId + '\'' +
+                ", hireDate=" + hireDate +
+                ", entDate=" + entDate +
+                ", entyn='" + entyn + '\'' +
+                '}';
+    }
+}
