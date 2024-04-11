08093ef13581b6f30f10b2dea0f1cc40ec9f65d7
GTU9 <sangik9999@naver.com>
2024-04-11 22:05:39 +0900

    .

---
+++ lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section02/provider/SelectBuilderMapper.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.section02.provider;
+
+import com.ohgiraffers.common.MenuDTO;
+import com.ohgiraffers.common.SearchCriteria;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
+
+import java.util.List;
+
+public interface SelectBuilderMapper {
+
+    @Results(id="menuResultMap", value={
+            @Result(id = true , property = "code", column ="menu_code"),
+            @Result(property = "name", column ="menu_name"),
+            @Result(property = "price", column ="menu_price"),
+            @Result(property = "category", column ="category_code"),
+            @Result(property = "orderableStatus", column ="orderable_status"),
+    })
+    @SelectProvider(type = SelectBuilderProvider.class, method = "selectAllMenu")
+    List<MenuDTO> selectAllMenu();
+
+    @SelectProvider(type= SelectBuilderProvider.class, method = "searchMenuByCondition")
+    List<MenuDTO> searchMenuByCondition(SearchCriteria searchCriteria);
+}
