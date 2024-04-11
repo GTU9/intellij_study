a685f8311fe09c2bb81003307d5eb7ccf69b4d4a
GTU9 <sangik9999@naver.com>
2024-04-11 11:25:16 +0900

    chooseSubMenu(), foreachSubMenu(), trimSubMenu() 실행 메소드

--- lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
+++ lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/MenuService.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.session.SqlSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 import static com.ohgiraffers.common.Template.getSqlSession;
 
@@ -46,6 +47,45 @@ public class MenuService {
                 System.out.println(menu);
             }
         }else{
+            System.out.println("검색 결과가 존재하지 않습니다.");
+        }
+    }
+
+    public void searchMenuBySupCategory(SearchCriteria searchCriteria) {
+
+        SqlSession sqlSession=getSqlSession();
+        mapper=sqlSession.getMapper(DynamicSqlMapper.class);
+
+        List<MenuDTO> menuList=mapper.searchMenuBySupCategory(searchCriteria);
+
+        sqlSession.close();
+
+        if(menuList != null && menuList.size()>0){
+            for(MenuDTO menu : menuList){
+                System.out.println(menu);
+            }
+        }else{
+            System.out.println("검색 결과가 존재하지 않습니다.");
+        }
+    }
+
+
+    public void searchMenuByRandomMenuCode(List<Integer> randomMenuCodeList) {
+        SqlSession sqlSession=getSqlSession();
+        mapper=sqlSession.getMapper(DynamicSqlMapper.class);
+
+        Map<String, List<Integer>> criteria=new HashMap<>();
+        criteria.put("randomMenuCodeList",randomMenuCodeList);
+
+        List<MenuDTO> menuList=mapper.searchMenuByRandomMenuCode(criteria);
+
+        sqlSession.close();
+
+        if(menuList != null && menuList.size()>0){
+            for(MenuDTO menu : menuList){
+                System.out.println(menu);
+            }
+        }else {
             System.out.println("검색 결과가 존재하지 않습니다.");
         }
     }
