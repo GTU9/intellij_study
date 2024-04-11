e2067b224744185d47430fae266a6635c9957d98
GTU9 <sangik9999@naver.com>
2024-04-11 16:24:54 +0900

    .

--- lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/DynamicSqlMapper.java
+++ lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/DynamicSqlMapper.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public interface DynamicSqlMapper {
+
     List<MenuDTO> selectMenuByPrice(Map<String, Integer> map);
 
     List<MenuDTO> searchMenu(SearchCriteria searchCriteria);
@@ -14,4 +15,10 @@ public interface DynamicSqlMapper {
     List<MenuDTO> searchMenuBySupCategory(SearchCriteria searchCriteria);
 
     List<MenuDTO> searchMenuByRandomMenuCode(Map<String, List<Integer>> criteria);
+
+    List<MenuDTO> searchMenuByCodeOrSearchAll(SearchCriteria searchCriteria);
+
+    List<MenuDTO> searchMenuByNameOrCategory(Map<String, Object> criteria);
+
+    int modifyMenu(Map<String, Object> criteria);
 }
