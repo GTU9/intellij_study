a828983e8061e89f716e5f7f13cc5b51cb4b7e76
GTU9 <sangik9999@naver.com>
2024-04-11 11:25:01 +0900

    chooseSubMenu(), foreachSubMenu(), trimSubMenu() 실행UI

--- lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
+++ lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section01/xml/Application.java
@@ -2,7 +2,7 @@ package com.ohgiraffers.section01.xml;
 
 import com.ohgiraffers.common.SearchCriteria;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
 
@@ -15,13 +15,16 @@ public class Application {
             System.out.println("3. foreach test");
             System.out.println("4. trim(where, set) test");
             System.out.println("9. 프로그램 종료");
-            System.out.print(" 실행할 메뉴 번호를 입력하세요 : ");
+            System.out.print("실행할 메뉴 번호를 입력하세요 : ");
             int no =sc.nextInt();
 
 
             switch (no){
                 case 1:
                     infSubMenu(); break;
+                case 2: chooseSubMenu();break;
+                case 3: foreachSubMenu();break;
+                case 4: trimSubMenu(); break;
                 case 9:
                     System.out.println("프로그램을 종료합니다."); break;
             }
@@ -67,5 +70,68 @@ public class Application {
         String value = sc.nextLine();
 
         return new SearchCriteria (condtion, value);
+    }
+
+    private static void chooseSubMenu(){
+        Scanner sc= new Scanner(System.in);
+        MenuService menuService=new MenuService();
+
+        do{
+            System.out.println("==========================choose Sub Menu==============================");
+            System.out.println("1. 카테고리 상위 분류별 메뉴 목록 조회");
+            System.out.println("9. 이전 메뉴로 돌아가기 ");
+            System.out.print("실행할 메뉴를 입력하세요 :  ");
+            int no=sc.nextInt();
+
+            switch(no){
+                case 1: menuService.searchMenuBySupCategory(inputSupCategory()); break;
+                case 9: return;
+            }
+        }while(true);
+
+    }
+
+    private static SearchCriteria inputSupCategory(){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("상위 분류를 입력하세요 (식사 or 음료 or 디저트) : ");
+        String value=sc.nextLine();
+
+        return new SearchCriteria ("supCategory", value) ;
+    }
+
+    private static void foreachSubMenu(){
+        Scanner sc=new Scanner(System.in);
+        MenuService menuService=new MenuService();
+
+        do{
+            System.out.println("==========================choose Sub Menu==============================");
+            System.out.println("1. 랜덤한 메뉴 5개 추출 조회");
+            System.out.println("9. 이전 메뉴로 돌아가기 ");
+            System.out.print("실행할 메뉴를 입력하세요 :  ");
+            int no=sc.nextInt();
+
+            switch(no){
+                case 1: menuService.searchMenuByRandomMenuCode(createRandomMenuCodeList()); break;
+                case 9: return;
+            }
+        }while(true);
+    }
+
+    public static List<Integer> createRandomMenuCodeList(){
+        Set<Integer> set =new HashSet<>();
+        while (set.size()<5){
+            int temp=((int)(Math.random()*20)+1);
+            set.add(temp);
+        }
+
+        List<Integer> list = new ArrayList<>(set);
+        Collections.sort(list);
+
+        return list;
+    }
+
+    private static void trimSubMenu() {
+        Scanner sc=new Scanner(System.in);
+        MenuService menuService=new MenuService();
     }
 }
