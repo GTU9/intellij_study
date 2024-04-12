a397c491b1aa5f84d87d5284ddebee3c92d2b858
GTU9 <sangik9999@naver.com>
2024-04-12 09:43:01 +0900

    main Application

--- lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section02/provider/Application.java
+++ lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section02/provider/Application.java
@@ -5,45 +5,47 @@ import com.ohgiraffers.common.SearchCriteria;
 import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        do{
-            System.out.println("=====================구문 빌더 API를 이용한 Dynamic SQL======================");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("=========== 구문 빌더 API를 이용한 Dynamic SQL ===========");
             System.out.println("1. SelectBuilder test");
             System.out.println("2. SqlBuilder test");
             System.out.println("9. 프로그램 종료");
-            System.out.print("실행할 메뉴 번호를 입력해주세요");
-            int no=scanner.nextInt();
+            System.out.println("실행할 메뉴 번호를 입력해주세요 : ");
+            int no = sc.nextInt();
 
-            switch (no){
-                case 1: selectBuilderSubMenu();break;
-//                case 2: sqlBuilderSubMenu();break;
-                case 3: System.out.println("프로그램을 종료합니다."); return;
+            switch (no) {
+                case 1: selectBuilderSubMenu(); break;
+//                case 2: sqlBuilderSubMenu(); break;
+                case 9: System.out.println("프로그램을 종료합니다."); return;
             }
-
-        }while(true);
+        } while (true);
     }
 
-    private  static void selectBuilderSubMenu(){
-        Scanner scanner = new Scanner(System.in);
+    private static void selectBuilderSubMenu() {
+        Scanner sc = new Scanner(System.in);
         SelectBuilderService selectBuilderService = new SelectBuilderService();
-        do{
-            System.out.println("=====================구문 빌더 API를 이용한 Dynamic SQL======================");
+
+        do {
+            System.out.println("=========== SelectBuilder Sub Menu ===========");
             System.out.println("1. SelectBuilder 기본 구문 사용");
             System.out.println("2. SelectBuilder를 이용한 동적 쿼리 사용");
             System.out.println("9. 이전 메뉴로 이동");
             System.out.println("실행할 메뉴 번호를 입력해주세요 : ");
-            int no=scanner.nextInt();
+            int no = sc.nextInt();
 
-            switch (no){
-                case 1: selectBuilderService.testSimpleStatement();break;
+            switch (no) {
+                case 1: selectBuilderService.testSimpleStatement(); break;
                 case 2: selectBuilderService.testDynamicStatement(inputSearchCriteria()); break;
                 case 9: return;
             }
 
-        }while (true);
+        } while (true);
     }
+
     private static SearchCriteria inputSearchCriteria() {
         Scanner sc = new Scanner(System.in);
         System.out.println("검색 기준을 입력하세요 (category 또는 menu) : ");
