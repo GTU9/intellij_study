48a4c0bbf16d522310ce462258a88ad7574e265c
GTU9 <sangik9999@naver.com>
2024-04-12 09:42:44 +0900

    mapper

--- lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section02/provider/SelectBuilderMapper.java
+++ lecture-source/04_mybatis/chap04-dynamic-sql-lecture-source/src/main/java/com/ohgiraffers/section02/provider/SelectBuilderMapper.java
@@ -3,6 +3,7 @@ package com.ohgiraffers.section02.provider;
 import com.ohgiraffers.common.MenuDTO;
 import com.ohgiraffers.common.SearchCriteria;
 import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.SelectProvider;
 
@@ -10,16 +11,18 @@ import java.util.List;
 
 public interface SelectBuilderMapper {
 
-    @Results(id="menuResultMap", value={
-            @Result(id = true , property = "code", column ="menu_code"),
-            @Result(property = "name", column ="menu_name"),
-            @Result(property = "price", column ="menu_price"),
-            @Result(property = "category", column ="category_code"),
-            @Result(property = "orderableStatus", column ="orderable_status"),
+    @Results(id = "menuResultMap", value = {
+            @Result(id = true, property = "code", column= "menu_code"),
+            @Result(property = "name", column= "menu_name"),
+            @Result(property = "price", column= "menu_price"),
+            @Result(property = "categoryCode", column= "category_code"),
+            @Result(property = "orderableStatus", column= "orderable_Status"),
+
     })
     @SelectProvider(type = SelectBuilderProvider.class, method = "selectAllMenu")
     List<MenuDTO> selectAllMenu();
 
-    @SelectProvider(type= SelectBuilderProvider.class, method = "searchMenuByCondition")
+    @ResultMap("menuResultMap")
+    @SelectProvider(type = SelectBuilderProvider.class, method = "searchMenuByCondition")
     List<MenuDTO> searchMenuByCondition(SearchCriteria searchCriteria);
 }
