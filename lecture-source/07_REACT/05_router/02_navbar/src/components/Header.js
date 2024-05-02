65942be7120097ae873631c27fe6b2e1e21eeaa0
GTU9 <sangik9999@naver.com>
2024-05-02 13:55:18 +0900

    .

---
+++ lecture-source/07_REACT/05_router/02_navbar/src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(){
+    return(
+        <div>
+            <h1>Welcome To Veagan Party</h1>
+        </div>
+
+    );
+}
+export default Header;
---
+++ lecture-source/07_REACT/05_router/02_navbar/src/pages/Menu.js
@@ -0,0 +1,6 @@
+function Menu(){
+    return(
+        <h1>메뉴목록</h1>
+    );
+}
+export default Menu;
