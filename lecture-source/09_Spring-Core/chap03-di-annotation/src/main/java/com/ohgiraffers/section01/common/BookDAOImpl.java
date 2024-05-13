a73798bf8bd5f1e066635b8902f84a55ab9de275
GTU9 <sangik9999@naver.com>
2024-05-13 09:38:54 +0900

    BookDAO Implement Repasitory

---
+++ lecture-source/09_Spring-Core/chap03-di-annotation/src/main/java/com/ohgiraffers/section01/common/BookDAOImpl.java
@@ -0,0 +1,27 @@
+package com.ohgiraffers.section01.common;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository("bookDAO")
+public class BookDAOImpl implements BookDAO {
+
+    private Map<Integer,BookDTO> bookList;
+
+    public BookDAOImpl() {
+        bookList = new HashMap<Integer,BookDTO>();
+        bookList.put(1, new BookDTO(1, 123456,"아이는 무엇으로 자라는가", "버지니아 시티어", "포레스트 북스", new Date()));
+        bookList.put(2, new BookDTO(2, 654321,"세이노의  가르침", "세이노(SayNo)", "데이원", new Date()));
+    }
+
+    @Override
+    public List<BookDTO> selectBookList() {
+        return new ArrayList<>(bookList.values());
+    }
+
+    @Override
+    public BookDTO selectOneBook(int sequence) {
+        return bookList.get(sequence);
+    }
+}
