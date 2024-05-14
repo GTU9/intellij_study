2fa79b76e613be3a725e50fd4a789d1bed4050f4
GTU9 <sangik9999@naver.com>
2024-05-14 09:00:26 +0900

    Bean scope 활용

---
+++ lecture-source/09_Spring-Core/chap04-bean/src/main/java/com/ohgiraffer/section01/scope/sub01/singleton/ContextConfiguration.java
@@ -0,0 +1,35 @@
+package com.ohgiraffer.section01.scope.sub01.singleton;
+
+import com.ohgiraffer.common.Beverage;
+import com.ohgiraffer.common.Bread;
+import com.ohgiraffer.common.Product;
+import com.ohgiraffer.common.ShoppingCart;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class ContextConfiguration {
+
+    @Bean
+    public Product carpBread(){
+        return new Bread("붕어빵", 1500, new java.util.Date());
+    }
+
+    @Bean
+    public Product milk(){
+        return new Beverage("초코우유",1800, 300);
+    }
+
+    @Bean
+    public Product water(){
+        return new Beverage("지리산암반수", 2000,500);
+    }
+
+    @Bean
+    @Scope("singleton")
+    public ShoppingCart cart(){
+        return new ShoppingCart();
+    }
+
+}
