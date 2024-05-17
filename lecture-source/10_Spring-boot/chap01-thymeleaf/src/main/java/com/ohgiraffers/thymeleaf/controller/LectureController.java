21d02b0387d5f9b941c54e9d309ee6a89d9d717a
GTU9 <sangik9999@naver.com>
2024-05-17 13:31:10 +0900

    templates html 연결 controller

---
+++ lecture-source/10_Spring-boot/chap01-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.thymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("lecture")
+public class LectureController  {
+
+    @GetMapping("expression")
+    public ModelAndView expression(ModelAndView mv){
+
+        mv.setViewName("/lecture/expression");
+
+        return mv;
+    }
+}
