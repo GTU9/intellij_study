1607724685ab43d1c0b6357fb31b14df68d6d969
GTU9 <sangik9999@naver.com>
2024-05-17 09:38:53 +0900

    Spring-boot test기본설정

---
+++ lecture-source/10_Spring-boot/test-initializer/src/main/java/com/ohgiraffers/testinitializer/TestInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.testinitializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestInitializerApplication.class, args);
+	}
+
+}
---
+++ lecture-source/10_Spring-boot/test-initializer/src/test/java/com/ohgiraffers/testinitializer/TestInitializerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.testinitializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestInitializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
