6f306c1f5ec5893e68284c7ed15c3dc58cbe9ea9
GTU9 <sangik9999@naver.com>
2024-05-17 15:30:26 +0900

    html파일 bean에 연결

--- lecture-source/10_Spring-boot/chap01-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
+++ lecture-source/10_Spring-boot/chap01-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -1,5 +1,6 @@
 package com.ohgiraffers.thymeleaf.controller;
 
+import com.ohgiraffers.thymeleaf.model.dto.MemberDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +13,19 @@ public class LectureController  {
     @GetMapping("expression")
     public ModelAndView expression(ModelAndView mv){
 
+        mv.addObject("member", new MemberDTO("토끼",20,'무',"서울시 노원구"));
+
+        mv.addObject("hello", "hello <h3>Thymeleaf</h3>");
+
         mv.setViewName("/lecture/expression");
+
+        return mv;
+    }
+
+    @GetMapping("conditional")
+    public ModelAndView conditional(ModelAndView mv){
+
+        mv.setViewName("/lecture/conditional");
 
         return mv;
     }
