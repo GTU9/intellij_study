7f16482182558ac72f2070174d8236eed61651d7
GTU9 <sangik9999@naver.com>
2024-05-17 15:12:03 +0900

    .

--- lecture-source/10_Spring-boot/chap01-thymeleaf/src/test/java/com/ohgiraffers/thymeleaf/ApplicationTests.java
+++ lecture-source/10_Spring-boot/chap01-thymeleaf/src/test/java/com/ohgiraffers/thymeleaf/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Chap01ThymeleafApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
