9010f8b73ee48aad1d43b0761cf26c30f7d52b85
GTU9 <107167326+GTU9@users.noreply.github.com>
2024-05-18 21:33:42 +0900

    표현, 제어, etc 페이지 컨트롤러

--- lecture-source/10_Spring-boot/chap01-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
+++ lecture-source/10_Spring-boot/chap01-thymeleaf/src/main/java/com/ohgiraffers/thymeleaf/controller/LectureController.java
@@ -1,7 +1,9 @@
 package com.ohgiraffers.thymeleaf.controller;
 
 import com.ohgiraffers.thymeleaf.model.dto.MemberDTO;
+import com.ohgiraffers.thymeleaf.model.dto.SelectCriteria;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,14 +13,14 @@ import java.util.List;
 
 @Controller
 @RequestMapping("lecture")
-public class LectureController  {
+public class LectureController {
 
     @GetMapping("expression")
-    public ModelAndView expression(ModelAndView mv){
+    public ModelAndView expression(ModelAndView mv) {
 
-        mv.addObject("member", new MemberDTO("토끼",20,'무',"서울시 노원구"));
+        mv.addObject("member", new MemberDTO("토끼", 20, '무', "서울시 노원구"));
 
-        mv.addObject("hello", "hello <h3>Thymeleaf</h3>");
+        mv.addObject("hello", "hello <h3>Thymeleaf</h3>!");
 
         mv.setViewName("/lecture/expression");
 
@@ -26,15 +28,35 @@ public class LectureController  {
     }
 
     @GetMapping("conditional")
-    public ModelAndView conditional(ModelAndView mv){
+    public ModelAndView conditional(ModelAndView mv) {
 
         mv.addObject("num", 216);
         mv.addObject("str", "바나나");
 
-        List<MemberDTO> memberList=new ArrayList<>();
+        List<MemberDTO> memberList = new ArrayList<>();
+        memberList.add(new MemberDTO("이재혁", 25, '남', "서울시 노원구"));
+        memberList.add(new MemberDTO("도영익", 25, '남', "삼육대 기숙사"));
+        memberList.add(new MemberDTO("윤석용", 24, '남', "삼육대 에스라관"));
+        memberList.add(new MemberDTO("김상익", 32, '남', "삼육대 1번지"));
+
+        mv.addObject("memberList", memberList);
+
 
         mv.setViewName("/lecture/conditional");
 
         return mv;
     }
+
+    @GetMapping("etc")
+    public ModelAndView etc(ModelAndView mv) {
+
+        SelectCriteria selectCriteria = new SelectCriteria(1, 10, 7);
+
+        mv.addObject(selectCriteria);
+
+        mv.setViewName("/lecture/etc");
+
+        return mv;
+    }
+
 }
