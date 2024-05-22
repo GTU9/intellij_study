01ee0ffe6258c7b492b86e92d719a42ed6601b82
GTU9 <sangik9999@naver.com>
2024-05-22 15:57:06 +0900

    preHandle, postHandle, afterCompletion 메소드 override

--- lecture-source/10_Spring-boot/chap06-interceptor/src/main/java/com/ohgiraffers/interceptor/StopwatchInterceptor.java
+++ lecture-source/10_Spring-boot/chap06-interceptor/src/main/java/com/ohgiraffers/interceptor/StopwatchInterceptor.java
@@ -6,18 +6,22 @@ import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class StopwatchInterceptor implements HandlerInterceptor {
+
+    /*전처리 메소드*/
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        System.out.println("interceptor preHandle 호출");
     }
 
+    /*후처리 메소드*/
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+        System.out.println("interceptor postHandle 호출");
     }
 
+    /*마지막에 호출하는 메소드*/
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        System.out.println("");
     }
 }
--- lecture-source/10_Spring-boot/chap06-interceptor/src/main/java/com/ohgiraffers/interceptor/interceptorTestController.java
+++
@@ -1,4 +0,0 @@
-package com.ohgiraffers.interceptor;
-
-public class interceptorTestController {
-}
