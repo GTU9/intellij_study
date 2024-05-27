f382bee1bf64ac95f289aed328888232f2f33cb5
GTU9 <sangik9999@naver.com>
2024-05-27 13:57:42 +0900

    JPA Repository 활용

--- lecture-source/05_jpa/chap04-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/entity/Menu.java
+++ lecture-source/05_jpa/chap04-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/entity/Menu.java
@@ -21,5 +21,47 @@ public class Menu {
     private int categoryCode;
 
     @Column(name="orderable_status")
-    private String orderalbeStatus;
+    private String orderableStatus;
+
+    protected Menu() {}
+
+    public Menu(int menuCode, String menuName, int menuPrice, int categoryCode, String orderableStatus) {
+        this.menuCode = menuCode;
+        this.menuName = menuName;
+        this.menuPrice = menuPrice;
+        this.categoryCode = categoryCode;
+        this.orderableStatus = orderableStatus;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public int getMenuPrice() {
+        return menuPrice;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public String getOrderableStatus() {
+        return orderableStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "menuCode=" + menuCode +
+                ", menuName='" + menuName + '\'' +
+                ", menuPrice=" + menuPrice +
+                ", categoryCode=" + categoryCode +
+                ", orderableStatus='" + orderableStatus + '\'' +
+                '}';
+    }
+
 }
--- lecture-source/05_jpa/chap04-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/repository/MenuRepository.java
+++ lecture-source/05_jpa/chap04-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/repository/MenuRepository.java
@@ -3,5 +3,5 @@ package com.ohgiraffers.springdatajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MenuRepository extends JpaRepository {
-    
+
 }
