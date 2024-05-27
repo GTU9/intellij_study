c05c0446264748b417b22acb7d8716266298efc4
GTU9 <sangik9999@naver.com>
2024-05-27 13:52:13 +0900

    JPA Repository 활용

---
+++ lecture-source/05_jpa/chap04-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/menu/entity/Menu.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.springdatajpa.menu.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table
+public class Menu {
+
+    @Id
+    @Column(name="menu_code")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int menuCode;
+
+    @Column(name="menu_name")
+    private String menuName;
+
+    @Column(name="menu_price")
+    private int menuPrice;
+
+    @Column(name="category_code")
+    private int categoryCode;
+
+    @Column(name="orderable_status")
+    private String orderalbeStatus;
+}
---
+++ lecture-source/05_jpa/chap04-spring-data-jpa/src/main/java/com/ohgiraffers/springdatajpa/repository/MenuRepository.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.springdatajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuRepository extends JpaRepository {
+    
+}
