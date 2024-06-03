8e1c230a7b1b9f92e570ba761658ac6d51370ad8
GTU9 <sangik9999@naver.com>
2024-06-03 11:20:04 +0900

    Update AuthDetails.java

--- lecture-source/12_Spring_Security/chap01-session-security/src/main/java/com/ohgiraffers/sessionsecurity/auth/model/AuthDetails.java
+++ lecture-source/12_Spring_Security/chap01-session-security/src/main/java/com/ohgiraffers/sessionsecurity/auth/model/AuthDetails.java
@@ -25,6 +25,10 @@ public class AuthDetails implements UserDetails {
         this.loginUserDTO = loginUserDTO;
     }
 
+    /*
+    * 권한 정보를 반환하는 메서드이다.
+    * UsernamePasswordAuthenticationToken에 사용자의 권한 정보를 넣을 때 사용된다.
+    * */
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
 
@@ -34,31 +38,50 @@ public class AuthDetails implements UserDetails {
         return authorities;
     }
 
+
+    /*
+     * 사용자의 비밀번호를 반환하는 메서드이다.
+     * UsernamePasswordAuthenticationToken에 사용자의 비밀번호를 비교할 때 사용된다.
+     * */
     @Override
     public String getPassword() {
         return loginUserDTO.getPassword();
     }
 
+    /*
+     * 사용자의 아이디를 반환하는 메서드이다.
+     * UsernamePasswordAuthenticationToken에 사용자의를 아이디를 비교할 때 사용된다.
+     * */
     @Override
     public String getUsername() {
         return loginUserDTO.getUserName();
     }
 
+    /*계정 만료 여부를 표현하는 메서드로 false이면 해당 계정을 사용할 수 없다.*/
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    /*
+     * 잠겨있는 계정을 확인하는 메서드로 false이면 해당 계정을 사용할 수 없다.
+     * 비밀번호 반복 실패로 일시적인 계정 lock의 경우 혹은 오랜 기간 비 접속으로 휴면 처리
+     * */
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    /*
+     * 탈퇴 계정 여부를 표현하는 메서드 false면 해당 계정을 사용할 수 없다.
+     * 보통 데이터 삭제는 즉시 하는 것이 아닌 일정 기간 보관 후 삭제한다.
+     * */
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    /* 계정 비활성화 여부로 사용자가 사용할 수 없는 상태 false이면 계정을 사용할 수 없다. */
     @Override
     public boolean isEnabled() {
         return true;
