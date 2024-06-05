1296f54e175a79514be017fed0feed709545c7e4
GTU9 <sangik9999@naver.com>
2024-06-05 09:45:29 +0900

    .

--- lecture-source/12_Spring_Security/chap02-jwt-security/src/main/java/com/ohgiraffers/jwtsecurity/user/controller/TestController.java
+++ lecture-source/12_Spring_Security/chap02-jwt-security/src/main/java/com/ohgiraffers/jwtsecurity/user/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.ohgiraffers.jwtsecurity.user.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@PreAuthorize("hasAuthority('ADMIN')")
 public class TestController {
 
     @GetMapping("/test")
