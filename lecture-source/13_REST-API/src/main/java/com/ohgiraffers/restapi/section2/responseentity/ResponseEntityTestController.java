a20ced265952ce78326c958ef0e7845a3b8f608d
GTU9 <sangik9999@naver.com>
2024-06-07 13:06:28 +0900

    responseentity

--- lecture-source/13_REST-API/src/main/java/com/ohgiraffers/restapi/section2/responseentity/ResponseEntityTestController.java
+++ lecture-source/13_REST-API/src/main/java/com/ohgiraffers/restapi/section2/responseentity/ResponseEntityTestController.java
@@ -4,11 +4,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.PrintStream;
+import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -49,7 +48,50 @@ public class ResponseEntityTestController {
 
     @GetMapping("/users/{userNo}")
     public ResponseEntity<ResponseMessage> findUserByNo(@PathVariable int userNo){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
         UserDTO foundUser=users.stream().filter(user->user.getNo()==userNo)
                                 .collect(Collectors.toList()).get(0);
+
+        Map<String, Object> responseMap = new HashMap<>();
+        responseMap.put("user",foundUser);
+
+        return ResponseEntity.ok().headers(headers).body(new ResponseMessage(200,"조회 성공",responseMap));
+
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> registerUser(@RequestBody UserDTO newUser){
+        System.out.println("[nesUser]"+newUser);
+
+        int lastUserNo=users.get(users.size()-1).getNo();
+        newUser.setNo(lastUserNo+1);
+
+        users.add(newUser);
+
+        return ResponseEntity.created(URI.create("/entity/users/"+users.get(users.size()-1).getNo()))
+                .build();
+    }
+
+    @PutMapping("/users/{userNo}")
+    public ResponseEntity<?> modifyUser(@PathVariable int userNo, @RequestBody UserDTO modifyInfo){
+        UserDTO foundUser=users.stream().filter(user->user.getNo()==userNo).
+                collect(Collectors.toList()).get(0);
+
+        foundUser.setId(modifyInfo.getId());
+        foundUser.setPwd(modifyInfo.getPwd());
+        foundUser.setName(modifyInfo.getName());
+
+        return ResponseEntity.created(URI.create("/entity/users/"+userNo)).build();
+    }
+
+    @DeleteMapping("/users/{userNo}")
+    public ResponseEntity<?> removeUser(@PathVariable int userNo){
+        UserDTO foundUser=users.stream().filter(user->user.getNo()==userNo)
+                .collect(Collectors.toList()).get(0);
+        users.remove(foundUser);
+
+        return ResponseEntity.noContent().build();
     }
 }
